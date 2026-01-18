ab92df887abb8116b2334e355784215076e4d0af
kenny <152108677+kennyh0522@users.noreply.github.com>
2026-01-18 00:08:09 -0600

    added way to read parquet files and updated directory in preprocessing to read parquets

---
+++ src/parquetParser.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import pyarrow.parquet as pq
+import glob
+from sys import getsizeof 
+
+print("Running...")
+
+# Parquet files
+parquet_files = glob.glob("data/Parquets")
+
+# List to store DataFrames
+dfs = []
+memory = 0
+
+# Read each Parquet file and convert it to a Pandas DataFrame
+for file in parquet_files:
+
+    table = pq.read_table(file)
+    df = table.to_pandas()  # Convert PyArrow Table to Pandas DataFrame
+    dfs.append(df)
+    memory += getsizeof(df)
+    if memory > 1E9:
+        # dfs takes a gigabyte of memory
+        break
+
+# Concatenate all DataFrames into one large DataFrame
+combined_df = pd.concat(dfs, ignore_index=True)
+
+# Verify the result by inspecting the first few rows
+combined_df.info(verbose=True)
+
+combined_df.to_csv("data/parquets/raw_parquet.csv", index = False)
+print("done")
